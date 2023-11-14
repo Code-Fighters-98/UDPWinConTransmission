669504d26c1c351a8305ecaf467fccb8a8f483ef
Emil Issaeian <code.fighters98@gmail.com>
2023-11-14 15:57:39 +0330

    Implement URP Client

--- UDP_PreDefinedEncoding/UDP_Client/Form1.Designer.cs
+++ UDP_PreDefinedEncoding/UDP_Client/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,205 +23,209 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            radioButton1 = new RadioButton();
+            textBox3 = new TextBox();
+            label9 = new Label();
+            SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.button1.Location = new System.Drawing.Point(459, 338);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(291, 100);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Execute";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(459, 422);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(291, 125);
+            button1.TabIndex = 2;
+            button1.Text = "Execute";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label1.Location = new System.Drawing.Point(12, 90);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 31);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "ID";
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 112);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 31);
+            label1.TabIndex = 3;
+            label1.Text = "ID";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label2.Location = new System.Drawing.Point(12, 162);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 31);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Name";
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(12, 202);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 31);
+            label2.TabIndex = 4;
+            label2.Text = "Name";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label3.Location = new System.Drawing.Point(12, 246);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 31);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "IsMale";
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 308);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 31);
+            label3.TabIndex = 5;
+            label3.Text = "IsMale";
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox1.Location = new System.Drawing.Point(148, 99);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(218, 30);
-            this.textBox1.TabIndex = 6;
+            textBox1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(148, 124);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(218, 30);
+            textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox2.Location = new System.Drawing.Point(148, 171);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(218, 30);
-            this.textBox2.TabIndex = 7;
+            textBox2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(148, 214);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(218, 30);
+            textBox2.TabIndex = 7;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label4.Location = new System.Drawing.Point(142, 46);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 31);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Set";
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(142, 58);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 31);
+            label4.TabIndex = 3;
+            label4.Text = "Set";
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label5.Location = new System.Drawing.Point(453, 46);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 31);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Get";
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(453, 58);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 31);
+            label5.TabIndex = 3;
+            label5.Text = "Get";
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label6.Location = new System.Drawing.Point(453, 100);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(86, 31);
-            this.label6.TabIndex = 3;
-            this.label6.Text = "IDGet";
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(453, 125);
+            label6.Name = "label6";
+            label6.Size = new Size(86, 31);
+            label6.TabIndex = 3;
+            label6.Text = "IDGet";
             // 
             // label7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label7.Location = new System.Drawing.Point(453, 163);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(130, 31);
-            this.label7.TabIndex = 4;
-            this.label7.Text = "NameGet";
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(453, 204);
+            label7.Name = "label7";
+            label7.Size = new Size(130, 31);
+            label7.TabIndex = 4;
+            label7.Text = "NameGet";
             // 
             // label8
             // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label8.Location = new System.Drawing.Point(453, 240);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(138, 31);
-            this.label8.TabIndex = 5;
-            this.label8.Text = "IsMaleGet";
+            label8.AutoSize = true;
+            label8.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(453, 300);
+            label8.Name = "label8";
+            label8.Size = new Size(138, 31);
+            label8.TabIndex = 5;
+            label8.Text = "IsMaleGet";
             // 
             // radioButton1
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(148, 253);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(17, 16);
-            this.radioButton1.TabIndex = 8;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(148, 316);
+            radioButton1.Margin = new Padding(3, 4, 3, 4);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(17, 16);
+            radioButton1.TabIndex = 8;
+            radioButton1.TabStop = true;
+            radioButton1.UseVisualStyleBackColor = true;
             // 
             // textBox3
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox3.Location = new System.Drawing.Point(148, 383);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(218, 30);
-            this.textBox3.TabIndex = 7;
+            textBox3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(148, 479);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(218, 30);
+            textBox3.TabIndex = 7;
             // 
             // label9
             // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label9.Location = new System.Drawing.Point(12, 383);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(64, 31);
-            this.label9.TabIndex = 3;
-            this.label9.Text = "Port";
+            label9.AutoSize = true;
+            label9.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(12, 479);
+            label9.Name = "label9";
+            label9.Size = new Size(64, 31);
+            label9.TabIndex = 3;
+            label9.Text = "Port";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 562);
+            Controls.Add(radioButton1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label3);
+            Controls.Add(label7);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label6);
+            Controls.Add(label9);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label9;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private RadioButton radioButton1;
+        private TextBox textBox3;
+        private Label label9;
     }
 }
-
--- UDP_PreDefinedEncoding/UDP_Client/Form1.cs
+++ UDP_PreDefinedEncoding/UDP_Client/Form1.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System;
+using System.Net;
 using Data;
+using System.Xml.Serialization;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Net.Sockets;
 
 namespace UDP_Client
 {
@@ -16,6 +14,7 @@ namespace UDP_Client
         public Form1()
         {
             InitializeComponent();
+
             label6.Text = string.Empty;
             label7.Text = string.Empty;
             label8.Text = string.Empty;
@@ -27,48 +26,70 @@ namespace UDP_Client
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int id = int.Parse(textBox1.Text.ToString());
-            string name = textBox2.Text.ToString();
-            bool isMale = radioButton1.Checked;
-
-            Person person = new Person(id, name, isMale);
-            
-
-        }
-
-        /*
-            if (args.Length < 2 || args.Length > 3)
-                throw new ArgumentException("Parameters : <Server> <Message> [<Port>]");
-
-            string server = args[0];
-            string message = args[1];
-            int port = args.Length == 3 ? int.Parse(args[2]) : 7;
-
-            byte[] FIFOBytePacket = Encoding.ASCII.GetBytes(args[1]);
-            UdpClient udpClient = null!;
 
             try
             {
-                udpClient = new UdpClient();
+                //Data Input Checking 
+                if (!(textBox1.Text.Length > 0 && textBox2.Text.Length > 0))
+                    throw new ArgumentException("Text Boxes Are Empty");
 
-                udpClient.Send(FIFOBytePacket, FIFOBytePacket.Length, server, port);
-                System.Console.WriteLine($"Sent {FIFOBytePacket.Length} bytes to the server...");
-                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
-                byte[] bytes = udpClient.Receive(ref iPEndPoint);
-                Console.WriteLine("Recieved {0} bytes from {1} : {2}",bytes.Length,iPEndPoint,Encoding.ASCII.GetString(bytes,0,bytes.Length));
+                int port = textBox3.Text.Length > 0 ?
+                     int.Parse(textBox3.Text) : 7;
+
+                if (!(port < IPEndPoint.MaxPort && port > IPEndPoint.MinPort))
+                    throw new ArgumentException("Port Range [0-65535]");
+
+                textBox3.Text = port.ToString();
+
+                int id = int.Parse(textBox1.Text);
+                string name = textBox2.Text;
+                bool isMale = radioButton1.Checked;
+
+                //Object initialized based on data input
+                Person personSent = new Person(id, name, isMale);
+
+
+                string personJSSent = JsonSerializer.Serialize<Person>(personSent);
+                byte[] packetSend = Encoding.Default.GetBytes(personJSSent); //could use UTF8 or other inputing based on user input
+
+
+                UdpClient udpClient = null!;
+
+                try
+                {
+
+                    udpClient = new UdpClient();
+                    udpClient.Send(packetSend, packetSend.Length, "127.0.0.1", port); //data sent
+
+
+                    IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0); //get from any ip and port
+                    byte[] packetReceived = udpClient.Receive(ref iPEndPoint); //data received
+                                                                               //
+                    string personJSReceived = Encoding.Default.GetString(packetReceived, 0, packetReceived.Length); //Decode
+
+                    //Deserialize
+                    Person personRecieved = JsonSerializer.Deserialize<Person>(personJSReceived)!;
+
+
+                    label6.Text = personRecieved.Id.ToString();
+                    label7.Text = personRecieved.Name;
+                    label8.Text = personRecieved.IsMale.ToString();
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine(se.ErrorCode + " : " + se.Message);
+                    Environment.Exit(se.ErrorCode);
+                }
+                finally
+                {
+                    udpClient?.Close();
+                }
+                Console.Read();
             }
-            catch (SocketException se)
+            catch (Exception ex)
             {
-                Console.WriteLine(se.ErrorCode + " : " + se.Message);
-                Environment.Exit(se.ErrorCode);
+                MessageBox.Show(ex.Message);
             }
-            finally
-            {
-                udpClient?.Close();
-            }
-            Console.Read();
-         */
-
-
+        }
     }
 }
