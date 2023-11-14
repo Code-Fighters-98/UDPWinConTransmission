d7b27c25f5565bb31b27432c12c54d169bb70839
Emil Issaeian <code.fighters98@gmail.com>
2023-11-14 17:33:59 +0330

    Add custome encoded person Udp Client Console App

---
+++ UDP_CustomEncoding/Udp_Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using Data;
+using EncoderDecoder;
+
+namespace Udp_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "127.0.0.1";
+            int port = args.Length == 1 ? int.Parse(args[1]) : 7;
+            Person p = new Person(12, "Emil",false);
+
+
+            UdpClient client = new UdpClient();
+            IPersonEncode encoder = new PersonEncoderText();
+            byte[] codedPerson = encoder.Encode(p);
+
+            client.Send(codedPerson, codedPerson.Length, server, port);
+
+            client.Close(); Console.Read();
+
+        }
+    }
+}
