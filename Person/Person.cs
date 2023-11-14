37cb0f752be02cf62a94cee7c1cb46292b9f40d0
Emil Issaeian <code.fighters98@gmail.com>
2023-11-14 13:29:55 +0330

    Add Person class lib

---
+++ Person/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace Data
+{
+    public class Person
+    {
+        private int id;
+        private string name;
+        private bool isMale;
+
+        public int Id { get { return id; } }
+        public string Name { get { return name; } set { name = value; } }
+        public bool IsMale { get { return isMale; } }
+
+        public Person(int id, string name, bool isMale)
+        {
+            this.id = id;
+            this.name = name;
+            this.isMale = isMale;
+        }
+
+        public Person()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} : {Name} : {IsMale}";
+        }
+    }
+}
--- UDP_PreDefinedEncoding/UDP_Client/Form1.cs
+++ UDP_PreDefinedEncoding/UDP_Client/Form1.cs
@@ -26,7 +26,7 @@ namespace UDP_Client
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         /*
