a6564346e36c2dbdc010a7ee8d87794e3ffabcb7
Emil Issaeian <code.fighters98@gmail.com>
2023-11-14 17:34:23 +0330

    Add custome encoded person Udp Server Console App

---
+++ UDP_CustomEncoding/Udp_Server/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Data;
+using EncoderDecoder;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Udp_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+                throw new ArgumentException("Parameters: <Port>");
+
+            int port = args.Length == 1 ? int.Parse(args[0]) : 7;
+
+            UdpClient udpClient = new UdpClient(port);
+            byte[] packet = new byte[PersonTextConst.MAX_WIRE_LENGTH];
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, port);
+            packet = udpClient.Receive(ref remoteIpEndPoint);
+            PersonDecoderText decoderText = new PersonDecoderText();
+
+            Person p = decoderText.Decode(packet);
+            System.Console.WriteLine(p);
+
+            udpClient.Close();
+            Console.Read();
+        }
+    }
+}
